4885e357cd3462ca496514991ba3f2587f096768
Mishan20 <ishansenanayaka1@gmail.com>
2025-03-18 21:00:55 +0530

    initial commit

---
+++ src/main/java/com/zerfinit/zerfinit_Api/ZerfinitApiApplication.java
@@ -0,0 +1,13 @@
+package com.zerfinit.zerfinit_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerfinitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZerfinitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerfinit/zerfinit_Api/ZerfinitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerfinit.zerfinit_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZerfinitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
